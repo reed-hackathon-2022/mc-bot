ec7528c0b15797dcaba5df0a01607a7736c33ebf
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-01-22 14:35:23 -0800

    Add __repr__ to MC object

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import json
 
-keys = json.load("tokens.json")
+#keys = json.load("tokens.json")
 
 
 class MissedConnection:
     def __init__(self, contents, image = None):
         self.contents = contents
         self.image = image
+
+    def __repr__(self):
+        return self.contents
