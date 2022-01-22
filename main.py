c0f72b104d82484488f9d9335f1d5006fb88f6f7
Patrick Norton <patrick.147.norton@gmail.com>
2022-01-22 13:41:46 -0800

    Added MissedConnection class

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import json
 
 keys = json.load("tokens.json")
+
+
+class MissedConnection:
+    def __init__(self, contents, image = None):
+        self.contents = contents
+        self.image = image
