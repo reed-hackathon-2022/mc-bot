093a73ab4dbd078978846e67b2044a333ad69eec
Sima Nerush <2002ssn@gmail.com>
2022-01-23 10:16:57 -0800

    Addd Tweeter function

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import json
+from parser import parse
+from tweeter import tweet_connections
 
 with open("tokens.json") as tokens:
     keys = json.load(tokens)
 
 
 class MissedConnection:
-    def __init__(self, contents, image = None):
+    def __init__(self, contents, image=None):
         self.contents = contents
         self.image = image
 
     def __repr__(self):
         return self.contents
+
+
+def launch_bot():
+    contents = parse()
+    tweet_connections(contents)
