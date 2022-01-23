5172ae9ecbd384fee8c08e2fb257c8dc0609466b
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-01-22 17:14:09 -0800

    Add empty MC helper function

--- parser.py
+++ parser.py
@@ -20,7 +20,22 @@ def is_MC_start(string):
     while is_int(string[i]):
         i += 1
     if string[i: i + 2] == '. ' and i != 0:    return True
-    else:                           return False
+    else:                                      return False
+
+#
+# Helper function that takes a MC as a string and returns True if MC is empty
+# (this happens if the MC only contained an image).
+#
+def is_empty(MC):
+    i = 0
+    while is_int(MC[i]):
+        i += 1
+    if MC[i + 1] == '.':
+        i += 1
+    MC = MC[i + 1:]
+    if MC.isspace():    return True
+    else:               return False
+
 
 #
 # parse(): Main parsing function. Scans through each line of MC.txt file to
@@ -41,16 +56,16 @@ def parse():
             foundMCs = True
         # gotten past Mack introduction, looking at MCs
         if foundMCs:
+            # found new MC
             if is_MC_start(lines[i]):
-                # found new MC
                 newMC = ''
                 j = i
                 # add all lines to MC
                 while j + 1< len(lines) and not is_MC_start(lines[j + 1]):
                     newMC += lines[j]
                     j += 1
-                # filter out MCs with sensitive information
-                if not sensitiveInfoChecker(newMC):
+                # filter out blank MCs (happens if MC is just an image) and MCs with sensitive information
+                if not is_empty(newMC) and not sensitiveInfoChecker(newMC):
                     newMCObject = main.MissedConnection(contents=newMC)
                     mcs.append(newMCObject)
             i = j + 1
