c1beaefc49ceece1777c02a85aa0512f273aa0b9
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-01-23 12:22:36 -0800

    Parsing bug fix

--- parser.py
+++ parser.py
@@ -85,7 +85,7 @@ def parse():
                         print(newMCArray)
                         print(len(newMCArray))
                         print()
-                        newMCObject = MissedConnection(contents=[newMC])
+                        newMCObject = MissedConnection(contents=newMC)
                     else:
                         newMCObject = MissedConnection(contents=[newMC])
                     mcs.append(newMCObject)
